a0c0ed017b8a36145939fbc970b77d4d4568ef41
bachtangthang <thang5217113@gmail.com>
2022-07-18 09:26:19 +0700

    fixing

--- sub.js
+++ sub.js
@@ -4,20 +4,20 @@ const grpc = require('grpc');
 const pubSubClient = new PubSub({grpc, projectId: "esoteric-bruin-356302"});
 require('dotenv').config();
 
+let messageCount = 0;
 const messageHandler = message => {
   console.log(`Received message ${message.id}:`);
   console.log(`Data: ${message.data}`);
   console.log(`Attributes: ${message.attributes}`);
   console.log('Received message:', JSON.parse(message.data.toString()));
-
+  messageCount+=1;
   // "Ack" (acknowledge receipt of) the message
   message.ack();
 };
 
 function listenForMessages() {
-  const subscription = pubSubClient.topic(process.env.topicName).subscription(process.env.subscriptionName);
+  const subscription = pubSubClient.subscription(process.env.subscriptionName);
 
-  let messageCount = 0;
   const timeout = 60;
   subscription.on('message', messageHandler);
 
